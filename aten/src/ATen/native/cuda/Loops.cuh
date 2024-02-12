61cf398a2be83b4f6da6502eb6f084a912e20805
mantaionut <ionut@janeasystems.com>
2024-02-12 20:08:49 +0200

    another try

--- aten/src/ATen/native/cuda/Loops.cuh
+++ aten/src/ATen/native/cuda/Loops.cuh
@@ -239,7 +239,7 @@ void opmath_symmetric_gpu_kernel_with_scalars(TensorIteratorBase& iter, const fu
     static int counter = 0;
     counter++;
     BEGIN_TIMER(gpu_kernel);
-    gpu_kernel(iter, BinaryFunctor<scalar_t, scalar_t, return_t, func_t>(f));
+    //gpu_kernel(iter, BinaryFunctor<scalar_t, scalar_t, return_t, func_t>(f));
     END_TIMER(gpu_kernel);
     if (counter == 750) {
       // std::cout << counter << " kernel\n";
