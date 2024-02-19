572dbf59aec55e1793f5353a18293a664f3e5ef2
mantaionut <ionut@janeasystems.com>
2024-02-19 17:00:41 +0200

    again

--- aten/src/ATen/native/cuda/CUDALoops.cuh
+++ aten/src/ATen/native/cuda/CUDALoops.cuh
@@ -58,7 +58,28 @@ __device__ unsigned long long counter_vectorized_elementwise_kernel;
 template <int vec_size, typename func_t, typename array_t>
 C10_LAUNCH_BOUNDS_1(num_threads())
 __global__ void vectorized_elementwise_kernel(int N, func_t f, array_t data) {
+using traits = function_traits<func_t>;
+  int remaining = N - block_work_size() * blockIdx.x;
 
+  if (remaining < block_work_size()) { // if this block handles the reminder,
+                                       // just do a naive unrolled loop
+    auto input_calc = TrivialOffsetCalculator<traits::arity>();
+    auto output_calc = TrivialOffsetCalculator<1>();
+    auto loader = memory::LoadWithoutCast();
+    auto storer = memory::StoreWithoutCast();
+    auto policy = memory::policies::unroll<
+        array_t,
+        decltype(input_calc),
+        decltype(output_calc),
+        memory::LoadWithoutCast,
+        memory::StoreWithoutCast>(
+        data, remaining, input_calc, output_calc, loader, storer);
+    elementwise_kernel_helper(f, policy);
+  } else { // if this block has a full `block_work_size` data to handle, use
+           // vectorized memory access
+    elementwise_kernel_helper(
+        f, memory::policies::vectorized<vec_size, array_t>(data));
+  }
 }
 
 template <
