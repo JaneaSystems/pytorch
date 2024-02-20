96b6e2133c4a628a3fdcbedd8b83e1835fe6a485
mantaionut <ionut@janeasystems.com>
2024-02-20 22:32:45 +0200

    another change

--- aten/src/ATen/native/cuda/CUDALoops.cuh
+++ aten/src/ATen/native/cuda/CUDALoops.cuh
@@ -57,7 +57,7 @@ __device__ unsigned long long device_time;
 __device__ unsigned long long counter_vectorized_elementwise_kernel;
 template <int vec_size, typename func_t>
 
-__global__ void vectorized_elementwise_kernel(int N, func_t f) {
+__global__ void vectorized_elementwise_kernel(int N) {
 
 }
 
@@ -102,8 +102,8 @@ static inline void launch_vectorized_kernel(
   if (vec_size == 4) {
     counter++;
     START_TIMER(gpu_kernel_vectorize);
-    vectorized_elementwise_kernel<4, func_t>
-        <<<grid, num_threads(), 0, nullptr>>>(N, f);
+    vectorized_elementwise_kernel<4>
+        <<<grid, num_threads(), 0, nullptr>>>(N);
     END_TIMER(gpu_kernel_vectorize);
     C10_CUDA_KERNEL_LAUNCH_CHECK();
   }
